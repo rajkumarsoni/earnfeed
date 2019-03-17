2c7ca46fa5588287d66a425dbf137e183579ac09
Rajkumar Soni <rajasoni1995@gmail.com>
2019-03-17 14:20:05 +0530

    Login with firebase completed

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,24 +1,36 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import firebase from "firebase";
 @IonicPage({
-  name: 'app-login-page'
+  name: "app-login-page"
 })
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+  selector: "page-login",
+  templateUrl: "login.html"
 })
 export class LoginPage {
+  email: string = "";
+  password: string = "";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+    console.log("ionViewDidLoad LoginPage");
   }
 
-  goToSignUpPage(){
-    this.navCtrl.push('app-sign-up-page');
+  login() {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.email, this.password)
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
+  goToSignUpPage() {
+    this.navCtrl.push("app-sign-up-page");
+  }
 }
--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -2,47 +2,47 @@ import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import firebase from "firebase";
 @IonicPage({
-  name: "app-sign-up-page"
+    name: "app-sign-up-page"
 })
 @Component({
-  selector: "page-sign-up",
-  templateUrl: "sign-up.html"
+    selector: "page-sign-up",
+    templateUrl: "sign-up.html"
 })
 export class SignUpPage {
-  name: string = "";
-  email: string = "";
-  password: string = "";
+    name: string = "";
+    email: string = "";
+    password: string = "";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+    constructor(public navCtrl: NavController, public navParams: NavParams) { }
 
-  ionViewDidLoad() {
-    console.log("ionViewDidLoad SignUpPage");
-  }
+    ionViewDidLoad() {
+        console.log("ionViewDidLoad SignUpPage");
+    }
 
-  signUp() {
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(this.email, this.password)
-      .then((data: any) => {
-        let newUser: firebase.User = data.user;
-        newUser
-          .updateProfile({
-            displayName: this.name,
-            photoURL: ""
-          })
-          .then(res => {
-            console.log("res", res);
-          })
-          .catch(err => {
-            console.log("err", err);
-          });
-        console.log(data);
-      })
-      .catch((error: any) => {
-        console.log(error);
-      });
-  }
-  goToLoginPage() {
-    this.navCtrl.pop();
-  }
+    signUp() {
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(this.email, this.password)
+            .then((data: any) => {
+                let newUser: firebase.User = data.user;
+                newUser
+                    .updateProfile({
+                        displayName: this.name,
+                        photoURL: ""
+                    })
+                    .then(res => {
+                        console.log("res", res);
+                    })
+                    .catch(err => {
+                        console.log("err", err);
+                    });
+                console.log(data);
+            })
+            .catch((error: any) => {
+                console.log(error);
+            });
+    }
+    goToLoginPage() {
+        this.navCtrl.pop();
+    }
 }
